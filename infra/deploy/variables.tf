50b54ecfb91d977cef43a40f97042c408b29355b
Jitu Singh <jitusingh18@gmail.com>
2026-04-25 06:44:42 +1200

    add terraform

---
+++ infra/deploy/variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+  description = "Prefix for resources in AWS"
+  default     = "raa"
+}
+
+variable "project" {
+  description = "Project name for tagging resources"
+  default     = "recipe-app-api"
+}
+
+variable "contact" {
+  description = "Conatact email for tagging"
+  default     = "jitusinghnz@gmail.com"
+}
